ceab9b3ea943406496eba3795fd373d774148163
joaqu1m <joaqu1m.pires@hotmail.com>
2025-03-23 19:51:36 +0000

    terraform ec2 sg and terraform init

---
+++ terraform/init-terraform.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+cd ./terraform
+
+terraform init
+terraform apply -auto-approve
--- terraform/startup/main.tf
+++ terraform/startup/main.tf
@@ -8,10 +8,29 @@ terraform {
   }
 }
 
+resource "aws_security_group" "terraform_runner_sg" {
+  name = "terraform-runner-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "terraform_runner" {
-  instance_type = "t3.micro"
-  key_name      = "iagiliza-key"
-  ami           = "ami-08b5b3a93ed654d19" // Amazon Linux 2023 AMI
+  instance_type   = "t3.micro"
+  key_name        = "iagiliza-key"
+  ami             = "ami-08b5b3a93ed654d19" // Amazon Linux 2023 AMI
+  security_groups = [aws_security_group.terraform_runner_sg.name]
 
   // TODO: Setup automático da máquina com CI/CD para rodar terraform. Isso inclui possivelmente criar e instanciar um serviço com Jenkins
   // Esperando aprovação do tema e definição do nome da org para prosseguir;
